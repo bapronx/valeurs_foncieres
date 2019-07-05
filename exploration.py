e5818a7d266d7964da1a2bc9ffa0719beb1dc4d0
bapronx <baptiste.rondeleux@cgg.com>
2019-07-05 20:06:30 +0200

    all step ready for geoloc submition

---
+++ exploration.py
@@ -0,0 +1,97 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'valeurs_foncieres'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# <a href="https://colab.research.google.com/github/bapronx/valeurs_foncieres/blob/master/valeurs_foncieres.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+#%% [markdown]
+# #Analyse des valeurs foncières
+#%% [markdown]
+# On commence par importer les modules d'analyse et télécharger les fichiers
+
+#%%
+import numpy as np
+import pandas as pd
+
+# get_ipython().system('wget https://www.data.gouv.fr/fr/datasets/r/1be77ca5-dc1b-4e50-af2b-0240147e0346')
+# fname = "1be77ca5-dc1b-4e50-af2b-0240147e0346"
+fname = "valeursfoncieres-2018.txt"
+
+fid = open(fname, 'r')
+df=pd.read_csv(fname,sep='|')
+fid.close()
+
+
+#%%
+df.sample(5)
+df.columns
+
+#%%
+def proc(s):
+  if isinstance(s, str):
+    return float(s.replace(',', '.'))
+  else:
+    return s
+
+
+def remove_out(d):
+  m = d.median()
+  s = np.abs(d-m).median()
+  selec = (d<=(m+2*s)) & (d>=(m-2*s))
+  return d[selec]
+
+#%%
+key_to_proc = ("Valeur fonciere", "Surface reelle bati", "Code postal")
+for key in key_to_proc:
+    df[key] = df[key].apply(proc)
+df["surf"] = df["Surface reelle bati"]
+df["prix"] = df["Valeur fonciere"]
+#%%
+selec = (df["Type local"]=="Appartement") & (df["Code departement"]==75) & (df["surf"]<200) & (df["prix"]<4e6) & (df["surf"]>20) & (df["prix"]>4e3)
+df["prix au m2"] = df["prix"] / df["surf"]
+
+selec2 = (df["Type local"]=="Appartement") & (df["Code departement"]==75)
+remove_out(df["prix au m2"][selec]).hist()
+
+selec_app = df["Type local"] == "Appartement"
+selec = selec_app & (df["Commune"] == "BOURG-EN-BRESSE")
+#%%
+
+def extract_address(df):
+    pass
+
+#%%
+import numpy as np
+
+#%%
+
+
+df["Code postal"][np.isnan(df["Code postal"])]=-9999
+def clean_str_nan(s):
+    if isinstance(s, float) and np.isnan(s):
+        return ""
+    return s
+df["Type de voie"] = df["Type de voie"].apply(clean_str_nan)
+df["prix au m2"][selec].describe()
+
+
+#%%
+remove_out(df["prix au m2"][selec]).describe()
+
+
+#%%
+
+
+
+#%%
+remove_out(df["prix au m2"][selec])
+
+
+#%%
+
+
+
--- geocoding.py
+++ geocoding.py
@@ -18,7 +18,7 @@ with open(keyfile, 'r') as f :
 
 print("Loaded key : {}".format(key))
 url = "http://www.mapquestapi.com/geocoding/v1/batch"
-data = {'location':["France, JASSERON , 01250,  SOUS LE BOIS GIROUD","France, Paris, 75009, 44 rue de dunkerque"], 'maxResults':1, 'key':key}
+#data = {'location':["France, JASSERON , 01250,  SOUS LE BOIS GIROUD","France, Paris, 75009, 44 rue de dunkerque"], 'maxResults':1, 'key':key}
 
 def submit_data(locations):
     """ Use mapquest api to request GPS coordinates from the address
@@ -26,17 +26,21 @@ def submit_data(locations):
     req = requests.get(url, params=locations)
     return json.loads(req.text)['results']
 
-def  line_to_location(serie):
+def  line_to_location_string(serie):
   number = serie["No voie"]
   if number.is_integer():
-    number = str(number)
+    number = str(int(number))
   else:
     number = ""
   return ", ".join(["France", \
       serie["Commune"], \
       ("%05i" % (serie["Code postal"],)), \
-        " ".join([number, serie["Voie"]])])
+        " ".join([ number, serie["Type de voie"], serie["Voie"]])])
 
+def get_all_location_string(df):
+  location = df.apply(line_to_location_string, axis=1)
+  return location.unique()
 
-toto = submit_data(data)
+from exploration import df
+toto = get_all_location_string(df)
 toto
